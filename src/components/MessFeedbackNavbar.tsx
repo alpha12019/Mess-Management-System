5b8bfd3f46e3d72f486581d497d952c80ef5315e
ANKIT12019 <rajankit12019@gmail.com>
2025-08-16 11:59:29 +0530

    commit

--- src/components/MessFeedbackNavbar.tsx
+++ src/components/MessFeedbackNavbar.tsx
@@ -4,8 +4,12 @@ import { NavLink } from 'react-router-dom';
 
 const MessFeedbackNavbar: React.FC = () => {
   const navIcons: Record<string, JSX.Element> = {
-    'Submit Feedback': <FaRegCommentDots className="mr-2" />,
-    'Applications': <FaClipboardList className="mr-2" />,
+    'Submit Feedback': <svg className="mr-2 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+      <path fillRule="evenodd" d="M18 10c0 3.866-3.582 7-8 7a8.841 8.841 0 01-4.083-.98L2 17l1.338-3.123C2.493 12.767 2 11.434 2 10c0-3.866 3.582-7 8-7s8 3.134 8 7zM7 9H5v2h2V9zm8 0h-2v2h2V9zM9 9h2v2H9V9z" clipRule="evenodd" />
+    </svg>,
+    'Applications': <svg className="mr-2 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+      <path fillRule="evenodd" d="M3 4a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zm0 4a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zm0 4a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zm0 4a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clipRule="evenodd" />
+    </svg>,
   };
 
   return (
