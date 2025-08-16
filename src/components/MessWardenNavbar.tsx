8b2da061ffa9808dc11a74889d5b774b220a70f9
ANKIT12019 <rajankit12019@gmail.com>
2025-08-16 11:58:11 +0530

    commit

--- src/components/MessWardenNavbar.tsx
+++ src/components/MessWardenNavbar.tsx
@@ -17,7 +17,9 @@ const MessWardenNavbar: React.FC = () => {
   const navIcons: Record<string, JSX.Element> = {
     'View Feedback | Statistics': <FaRegCommentDots className="mr-2" />,
     'View Menu': <FaUtensils className="mr-2" />,
-    'View Announcements': <FaVolumeUp className="mr-2" />,
+    'View Announcements': <svg className="mr-2 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+      <path fillRule="evenodd" d="M9.383 3.076A1 1 0 0110 4v12a1 1 0 01-1.617.793L5.5 15H3a1 1 0 01-1-1V6a1 1 0 011-1h2.5l3.883-2.793zM12.293 7.293a1 1 0 011.414 0L15 8.586l1.293-1.293a1 1 0 111.414 1.414L16.414 10l1.293 1.293a1 1 0 01-1.414 1.414L15 11.414l-1.293 1.293a1 1 0 01-1.414-1.414L13.586 10l-1.293-1.293a1 1 0 010-1.414z" clipRule="evenodd" />
+    </svg>,
     'View Registrations': <FaUserFriends className="mr-2" />,
     'View Bill And Payments': <FaMoneyBillWave className="mr-2" />,
   };
